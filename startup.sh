cf8fe7bde416a35d8d344b1725e84cb85ae02fc3
henriquearaujo <henriquearaujoo@gmail.com>
2021-11-06 09:39:42 -0300

    first commit

---
+++ startup.sh
@@ -0,0 +1,13 @@
+java -jar -Xms128m -Xmx128m -Xmn200m -Xss256k  cloud.jar&
+#java -jar -Xms512m -Xmx512m -Xmn200m -Xss256k  exchange.jar&
+#java -jar -Xms512m -Xmx512m -Xmn200m -Xss256k  market.jar&
+#java -jar -Xms128m -Xmx128m -Xmn200m -Xss256k  ucenter.jar&
+#java -jar -Xms512m -Xmx512m -Xmn200m -Xss256k  wallet.jar&
+#java -jar -Xms512m -Xmx512m -Xmn200m -Xss256k  chat.jar&
+#java -jar -Xms128m -Xmx128m -Xmn200m -Xss256k  otc-api.jar&
+#java -jar -Xms512m -Xmx512m -Xmn200m -Xss256k  exchange-api.jar&
+#java -jar -Xms512m -Xmx512m -Xmn200m -Xss256k  job.jar&
+java -jar -Xms128m -Xmx128m -Xmn200m -Xss256k  admin-.jar&
+#java -jar -Xms512m -Xmx512m -Xmn200m -Xss256k  bitcoin-1.2.jar&
+
+
